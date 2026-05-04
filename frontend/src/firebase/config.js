1b221b1d8d4695ce0890e03282b2fbf702d507dc
its_MartaBL <85373964+Hinata1411@users.noreply.github.com>
2026-05-04 17:49:52 -0600

    chore: configuracion de firebase en el proyecto
    Se integra Firebase con Firestore y Storage para persistencia de datos y archivos.

---
+++ frontend/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAaY8jpBn62hbs1OgZeDocV4IKhjG4Opk0",
+  authDomain: "asistente-uniformes.firebaseapp.com",
+  projectId: "asistente-uniformes",
+  storageBucket: "asistente-uniformes.firebasestorage.app",
+  messagingSenderId: "414923517330",
+  appId: "1:414923517330:web:ae9e6e6b9e4a01258b254b"
+};
+
+const app = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
+export const storage = getStorage(app)
